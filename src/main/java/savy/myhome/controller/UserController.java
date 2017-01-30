3f6efc2095807aadb82f5f3ad9ec5b656013ec9b
xiaoyangsavy <xiaoyangsavy@163.com>
2017-01-30 18:03:59 +0800

    加入shiro登录验证

--- src/main/java/savy/myhome/controller/UserController.java
+++ src/main/java/savy/myhome/controller/UserController.java
@@ -1,15 +1,26 @@
 package savy.myhome.controller;
 
+import java.security.MessageDigest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import savy.myhome.util.MD5Code;
 import savy.myhome.vo.User;
+import sun.misc.BASE64Encoder;
 
 /**
- *  控制器
- * @author AdminTC
+ * 用户
+ * @author Savvy
+ *
  */
 @Controller
 @RequestMapping(value="")
@@ -19,12 +30,27 @@ public class UserController {
 	 * 用户注册，只能接收POST请求
 	 */
 	@RequestMapping(method=RequestMethod.POST,value="/user/login")
-	public String registerMethod(User user,Model model) throws Exception{
+	public String registerMethod(User user,HttpServletRequest request) throws Exception{
+		
+		user.setPassword(new MD5Code().getMD5ofStr(user.getPassword()));
 		System.out.println("用户注册:" + user.toString());
-		//将user绑定到model对象中
-		model.addAttribute("user",user);
-		//转发到success.jsp页面
-		return "/success";
+		
+		Subject subject=SecurityUtils.getSubject();
+		UsernamePasswordToken token=new UsernamePasswordToken(user.getUsername(), user.getPassword());
+		try{
+			subject.login(token);
+			Session session=subject.getSession();
+			System.out.println("sessionId:"+session.getId());
+			System.out.println("sessionHost:"+session.getHost());
+			System.out.println("sessionTimeout:"+session.getTimeout());
+			session.setAttribute("info", "session的数据");
+			return "/success";
+		}catch(Exception e){
+			e.printStackTrace();
+			request.setAttribute("user", user);
+			request.setAttribute("errorMsg", "用户名或密码错误！");
+			return "/login";
+		}
 		
 	}
 }
